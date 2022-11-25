e5a833ab1eda6a3231b254ef20219d74a7f440a5
Денис Сухов <99960762+Fenickss@users.noreply.github.com>
2022-11-25 14:45:59 +0000

    +

---
+++ src/components/social-profile/Profile.jsx
@@ -0,0 +1,45 @@
+import user from './user.json'
+
+export default function User({user}) {
+    <div class="profile">
+  <div class="description">
+    <img
+      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+      alt="{user.username}"
+      class="avatar"
+    />
+    <p class="name">Petra Marica</p>
+    <p class="tag">@pmarica</p>
+    <p class="location">Salvador, Brasil</p>
+  </div>
+
+  <ul class="stats">
+    <li>
+      <span class="label">Followers</span>
+      <span class="quantity">1000</span>
+    </li>
+    <li>
+      <span class="label">Views</span>
+      <span class="quantity">2000</span>
+    </li>
+    <li>
+      <span class="label">Likes</span>
+      <span class="quantity">3000</span>
+    </li>
+  </ul>
+</div>
+}
+
+// ## Пример использования
+
+// ```js
+// import user from 'путь/к/user.json;
+
+// <Profile
+//   username={user.username}
+//   tag={user.tag}
+//   location={user.location}
+//   avatar={user.avatar}
+//   stats={user.stats}
+// />
+// ```
